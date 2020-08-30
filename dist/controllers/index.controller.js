27e10ba95d119ec3b614c6beefaa84cb583ae9af
codechappie <davidproyecto01@gmail.com>
2020-08-30 00:08:55 -0500

    main structure uploaded

--- dist/controllers/index.controller.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.indexWelcome = void 0;
-function indexWelcome(req, res) {
-    return res.json('Welcome ti mi API');
-}
-exports.indexWelcome = indexWelcome;
